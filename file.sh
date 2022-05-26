8938e419151204faba4efb84cc527012bacbd9b5
SunpreetKaur11 <87703984+SunpreetKaur11@users.noreply.github.com>
2022-05-26 19:53:01 +0530

    Delete file.sh

--- file.sh
+++
@@ -1,4 +0,0 @@
-echo "enter name of instance"
-read var 
-aws ec2 describe-instances --filters "Name=tag:Name,Values=$var" --query 'Reservations[].Instances[].{InstanceId:InstanceId,PublicIpAddress: PublicIpAddress}' | cat >> output.txt
-echo "saved Instance id and ip addresses on the basis of name"
